64faa4591a5a6a4707dfb196391c802728a7dbe6
ogMav8 <brad.anderson019@gmail.com>
2022-02-24 17:58:37 -0700

    moved bglogo to video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -5,12 +5,12 @@ import wesworldfpv_logo from '../../assets/images/wesworldfpv_logo.png'
 
 const Video = () => {
   return (
-    <div>
+    <div className='justify-content-center'>
 
     <div className="container-fluid mx-0 px-0" style={{ position: 'relative', zIndex: '1' }} >
         <video src={zionasfuck} width="100%"controls="controls" autoplay="true" loop="true"  muted="true"/>
     </div>
-      <div>
+      <div className="col">
         <img className="mx-auto mb-4 img-fluid img-reponsive logo_bg"  src={wesworldfpv_logo} alt="wesworldfpv_logo" />
       </div>
     </div>
